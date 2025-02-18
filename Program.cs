3157a9a4aae7f305bd900a64f0490f56dc7e4456
peeyush19 <piyush.thakur1999@outlook.com>
2025-02-18 17:57:43 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+namespace LINQ_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int[] age = { 25, 45, 16, 14, 19, 20, 24, 30 };
+
+            var a = from i in age where i > 20 select i;
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
