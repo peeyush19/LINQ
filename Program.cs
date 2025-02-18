b825fa5cd5b95cc851b5782f1c60858156402b98
peeyush19 <piyush.thakur1999@outlook.com>
2025-02-18 18:26:45 +0530

    string added

--- Program.cs
+++ Program.cs
@@ -9,9 +9,12 @@ namespace LINQ_Practice
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            int[] age = { 25, 45, 16, 14, 19, 20, 24, 30 };
+            // int[]age = { 25, 45, 16, 14, 19, 20, 24, 30 };
+            string[] names = { "Piyush", "Ajay ", "Jayprakash", "Chandru", "Priya" };
 
-            var a = from i in age where i > 20 select i;
+
+
+            var a = from i in names where i.Contains("u")  select i;
             foreach (var item in a)
             {
                 Console.WriteLine(item);
